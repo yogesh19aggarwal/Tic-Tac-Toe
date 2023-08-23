8471bda285446e6ca7d14e69fd50692e10b48b4c
Yogesh Aggarwal <yogesh.19.aggarwal@gmail.com>
2023-08-23 21:21:31 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 console.log("Welcome to Tic Tac Toe")
 
 let music = new Audio("music.mp3")
-let turn = new Audio("ting.mp3")
+let tune = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 
-let turn1 = "X"
+let turn = "X"
 
+// funtion to change the turn
 const changeTurn = ()=>{
-   return turn ===  "X"
+   return turn ===  "X" ? "0" : "X"
 }
+
+// function to check for a win
+const checkWin = ()=>{
+
+}
+
+// game logic
